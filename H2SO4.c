6e3e7666b9fff7d7ec3f8eb9c52cf897f798eb40
calebjbraun <Caleb Braun>
2016-04-25 20:53:52 -0500

    put a lock on our global counter, makes molecules correctly, does not release atoms correctly

--- H2SO4.c
+++ H2SO4.c
@@ -9,33 +9,48 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <errno.h>
-#include <time.h>
 #include "H2SO4.h"
 
-sem_t* oxy_sem;
-sem_t* hydro_sem;
-sem_t* sulf_sem;
-sem_t* mol_sem;
+sem_t* mol_oxy;
+sem_t* mol_hydro;
+sem_t* mol_sulf;
+
+sem_t* mol_sem;  // Indicates whether the atoms are allowed to leave
+sem_t* sum_lock;
+
+int mol_sum;
 
 void* oxygen(void* x) {
   printf("oxygen produced\n");
   fflush(stdout);
 
-  // Tell oxy_sem that there's one more O atom available
-  sem_post(oxy_sem);
+  // Allows the first four oxygens to go through
+  sem_wait(mol_oxy);
 
-  if (*oxy_sem < 5) {
-    sem_post(mol_sem);
-  }
+  // Aquires sum_lock in order to increment mol_sum
+  sem_wait(sum_lock);
+  mol_sum++;
+  sem_post(sum_lock);
 
-  if (*mol_sem == 7) {
-    sem_wait(oxy_sem);
-    printf("oxygen leaving \n");
+  printf("O Ready for Formation: %d\n", mol_sum);
+  fflush(stdout);
+
+  // Aquires sum_lcok in order to check it and maybe change its value
+  sem_wait(sum_lock);
+  if (mol_sum == 7) {
+    sem_post(mol_sem);  // Either one or zero
+    printf("\n*** Made H2SO4 Molecule! ***\n\n");
     fflush(stdout);
+    mol_sum = 0;
   }
+  sem_post(sum_lock);
 
-  // printf("oxygen leaving \n");
-  // fflush(stdout);
+  sem_wait(mol_sem);  // Waits for last atom in molecule
+  sem_post(mol_sem);  // Increments to allow other atoms to leave
+
+  sem_post(mol_oxy);
+  printf("oxygen leaving \n");
+  fflush(stdout);
   return (void*)0;
 }
 
@@ -43,11 +58,28 @@ void* hydrogen(void* x) {
   printf("hydrogen produced\n");
   fflush(stdout);
 
-  sem_post(hydro_sem);
-  if (*hydro_sem < 3) {
-    sem_post(mol_sem);
-  }
+  sem_wait(mol_hydro);  // 2 down to 0
 
+  sem_wait(sum_lock);
+  mol_sum++;
+  sem_post(sum_lock);
+
+  printf("H Ready for Formation: %d\n", mol_sum);
+  fflush(stdout);
+
+  sem_wait(sum_lock);
+  if (mol_sum == 7) {
+    sem_post(mol_sem);  // Either one or zero
+    printf("\n*** Made H2SO4 Molecule! ***\n\n");
+    fflush(stdout);
+    mol_sum = 0;
+  }
+  sem_post(sum_lock);
+
+  sem_wait(mol_sem);  // Waits for last atom in molecule
+  sem_post(mol_sem);  // Increments to allow other atoms to leave
+
+  sem_post(mol_hydro);
   printf("hydrogen leaving \n");
   fflush(stdout);
   return (void*)0;
@@ -57,15 +89,28 @@ void* sulfur(void* x) {
   printf("sulfur produced\n");
   fflush(stdout);
 
-  sem_post(sulf_sem);
-  if (*sulf_sem < 2) {
-    sem_post(mol_sem);
-  }
+  sem_wait(mol_sulf);  // 1 down to 0
 
-  // produce a water molecule
-  printf("\n*** Made H2SO4 Molecule! ***\n\n");
+  sem_wait(sum_lock);
+  mol_sum++;
+  sem_post(sum_lock);
+
+  printf("S Ready for Formation: %d\n", mol_sum);
   fflush(stdout);
 
+  sem_wait(sum_lock);
+  if (mol_sum == 7) {
+    sem_post(mol_sem);  // Either one or zero
+    printf("\n*** Made H2SO4 Molecule! ***\n\n");
+    fflush(stdout);
+    mol_sum = 0;
+  }
+  sem_post(sum_lock);
+
+  sem_wait(mol_sem);  // Waits for last atom in molecule
+  sem_post(mol_sem);  // Increments to allow other atoms to leave
+
+  sem_post(mol_sulf);
   printf("sulfur leaving \n");
   fflush(stdout);
   return (void*)0;
@@ -74,13 +119,13 @@ void* sulfur(void* x) {
 void openSems() {
   printf("open\n");
 
-  oxy_sem = sem_open("oxysem", O_CREAT | O_EXCL, 0466, 0);
-  while (oxy_sem == SEM_FAILED) {
+  mol_oxy = sem_open("moloxy", O_CREAT | O_EXCL, 0466, 4);
+  while (mol_oxy == SEM_FAILED) {
     if (errno == EEXIST) {
-      printf("semaphore oxysem already exists, unlinking and reopening\n");
+      printf("semaphore moloxy already exists, unlinking and reopening\n");
       fflush(stdout);
-      sem_unlink("oxysem");
-      oxy_sem = sem_open("oxysem", O_CREAT | O_EXCL, 0466, 0);
+      sem_unlink("moloxy");
+      mol_oxy = sem_open("moloxy", O_CREAT | O_EXCL, 0466, 4);
     } else {
       printf("semaphore could not be opened, error # %d\n", errno);
       fflush(stdout);
@@ -88,13 +133,13 @@ void openSems() {
     }
   }
 
-  hydro_sem = sem_open("hydrosem", O_CREAT | O_EXCL, 0466, 0);
-  while (hydro_sem == SEM_FAILED) {
+  mol_hydro = sem_open("molhydro", O_CREAT | O_EXCL, 0466, 2);
+  while (mol_hydro == SEM_FAILED) {
     if (errno == EEXIST) {
-      printf("semaphore hydrosem already exists, unlinking and reopening\n");
+      printf("semaphore molhydro already exists, unlinking and reopening\n");
       fflush(stdout);
-      sem_unlink("hydrosem");
-      hydro_sem = sem_open("hydrosem", O_CREAT | O_EXCL, 0466, 0);
+      sem_unlink("molhydro");
+      mol_hydro = sem_open("molhydro", O_CREAT | O_EXCL, 0466, 2);
     } else {
       printf("semaphore could not be opened, error # %d\n", errno);
       fflush(stdout);
@@ -102,13 +147,13 @@ void openSems() {
     }
   }
 
-  sulf_sem = sem_open("sulfsem", O_CREAT | O_EXCL, 0466, 0);
-  while (sulf_sem == SEM_FAILED) {
+  mol_sulf = sem_open("molsulf", O_CREAT | O_EXCL, 0466, 1);
+  while (mol_sulf == SEM_FAILED) {
     if (errno == EEXIST) {
-      printf("semaphore sulfsem already exists, unlinking and reopening\n");
+      printf("semaphore molsulf already exists, unlinking and reopening\n");
       fflush(stdout);
-      sem_unlink("sulfsem");
-      sulf_sem = sem_open("sulfsem", O_CREAT | O_EXCL, 0466, 0);
+      sem_unlink("molsulf");
+      mol_sulf = sem_open("molsulf", O_CREAT | O_EXCL, 0466, 1);
     } else {
       printf("semaphore could not be opened, error # %d\n", errno);
       fflush(stdout);
@@ -116,13 +161,27 @@ void openSems() {
     }
   }
 
-  mol_sem = sem_open("molsem", O_CREAT | O_EXCL, 0466, 7);
+  mol_sem = sem_open("molsem", O_CREAT | O_EXCL, 0466, 0);
   while (mol_sem == SEM_FAILED) {
     if (errno == EEXIST) {
       printf("semaphore molsem already exists, unlinking and reopening\n");
       fflush(stdout);
       sem_unlink("molsem");
-      mol_sem = sem_open("molsem", O_CREAT | O_EXCL, 0466, 7);
+      mol_sem = sem_open("molsem", O_CREAT | O_EXCL, 0466, 0);
+    } else {
+      printf("semaphore could not be opened, error # %d\n", errno);
+      fflush(stdout);
+      exit(1);
+    }
+  }
+
+  sum_lock = sem_open("sumlock", O_CREAT | O_EXCL, 0466, 1);
+  while (sum_lock == SEM_FAILED) {
+    if (errno == EEXIST) {
+      printf("semaphore sumlock already exists, unlinking and reopening\n");
+      fflush(stdout);
+      sem_unlink("sumlock");
+      sum_lock = sem_open("sumlock", O_CREAT | O_EXCL, 0466, 1);
     } else {
       printf("semaphore could not be opened, error # %d\n", errno);
       fflush(stdout);
@@ -133,12 +192,19 @@ void openSems() {
 
 void closeSems() {
   printf("close semaphores\n");
-  sem_close(hydro_sem);
-  sem_unlink("hydrosem\n");
-  sem_close(oxy_sem);
-  sem_unlink("oxysem\n");
-  sem_close(sulf_sem);
-  sem_unlink("sulfsem\n");
+
+  sem_close(mol_hydro);
+  sem_unlink("molhydro\n");
+
+  sem_close(mol_oxy);
+  sem_unlink("moloxy\n");
+
+  sem_close(mol_sulf);
+  sem_unlink("molsulf\n");
+
   sem_close(mol_sem);
   sem_unlink("molsem\n");
+
+  sem_close(sum_lock);
+  sem_unlink("sumlock\n");
 }
